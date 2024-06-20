300ef2769a99c34bf6e1ac27abb28aecda233ed1
owacunam <84757568+owacunam@users.noreply.github.com>
2024-06-19 22:34:33 -0600

    Boton Cita

--- POS_BeautySalon/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
+++ POS_BeautySalon/DAL/obj/Debug/net8.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f74515e63e1650f8e8be2df4ce596eb921d089f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ca1d15e3392337e886fc715b33465513a1d61a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
